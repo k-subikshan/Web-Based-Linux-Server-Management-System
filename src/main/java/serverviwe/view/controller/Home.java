09508aea6a680dd251676619c42d168768df5723
subikshan <k.subikshandevp@gmail.com>
2026-08-23 18:58:32 +0530

    adding dash borad

--- src/main/java/serverviwe/view/controller/Home.java
+++ src/main/java/serverviwe/view/controller/Home.java
@@ -1,6 +1,7 @@
 package serverviwe.view.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
